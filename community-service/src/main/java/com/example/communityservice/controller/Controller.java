8f188455b791655cd8310fbbc6b05be96b3d0b35
Tae Jun Park <162237157+tjpark312@users.noreply.github.com>
2024-12-04 22:47:59 +0900

    Test Commit

---
+++ community-service/src/main/java/com/example/communityservice/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.communityservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/community-service/posts")
+public class Controller {
+
+    @GetMapping
+    public String getPost(){
+        return "Hello World";
+    }
+}
